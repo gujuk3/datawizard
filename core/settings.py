d2a7d91eb8822ff1cffdda29d0bf6715d432401b
Ahmet Serdar Turan <serdar3turan@gmail.com>
2026-05-29 22:31:15 +0300

    email thingy login

--- core/settings.py
+++ core/settings.py
@@ -125,9 +125,10 @@ STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'build' / 'static']
 
 # Email (Gmail SMTP)
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default='False') == 'True'
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default='True') == 'True'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER', default='noreply@datawizard.com')
