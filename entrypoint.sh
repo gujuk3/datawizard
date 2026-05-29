6a4dc1a0b930f75501f9ef207a98fbe0d7acda0b
Ahmet Serdar Turan <serdar3turan@gmail.com>
2026-05-29 21:40:45 +0300

    production deployment config

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
 exec gunicorn core.wsgi:application \
-    --bind 0.0.0.0:8000 \
+    --bind 0.0.0.0:${PORT:-8000} \
     --workers 2 \
     --timeout 120 \
     --access-logfile - \
